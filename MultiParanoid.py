54220062856bddbb018e9b5be02e8a2e59ebc7f9
Bogdan <to.bogdan@gmail.com>
2013-10-14 19:11:53 +0200

    added the future MultiParanoid.py class file

---
+++ MultiParanoid.py
@@ -0,0 +1,16 @@
+'''
+See MultiParanoid class docs.
+'''
+
+
+class MultiParanoid(object):
+    '''
+    Incapsulates all access to the Multi/Quick-Paranoid results file.
+    Parses the file once upon initialization.
+    '''
+
+    def __init__(self, path):
+        '''
+        Parse the file.
+        '''
+        pass
