924b5025e1bf4efee4d9c22f261dda7533b3d576
Bogdan <to.bogdan@gmail.com>
2013-10-21 19:03:14 +0200

    implemented count_common_genes_subset()
    - new method: get_species_list()
    - new method: count_common_genes()
    - new method: gene2species()
    - all output goes through logging.info()

--- MultiParanoid.py
+++ MultiParanoid.py
@@ -26,10 +26,60 @@ class MultiParanoid(object):
         self.path = path
         self.no_tree_conflicts = no_tree_conflicts
         self.no_name_conficts = no_name_conflicts
+        # Full list of species found in the file.
+        self.species = set()
         # Mapping of each gene to the orthology clusterID(s) it belongs to.
         self.gene2ortho = {}
         # List of all the genes in the orthology cluster with given clusterID.
         self.ortho2genes = {}
+        self.parse()
+
+    def get_species_list(self):
+        '''
+        Returns the list of species found during parsing.
+        '''
+        return list(self.species)
+
+    def count_common_genes(self):
+        '''
+        Returns count of genes/orthologs common to (shared by) all the species in the
+        multiparanoid file.
+        '''
+        return self.count_common_genes_subset(self.species)
+
+    def count_common_genes_subset(self, species):
+        """
+        Counts genes/orthologs shared by the specified subset of species.
+        'species' is a list, each item is the GenBank LOCUS of the respective genome
+        (e.g. ['SAV_1680']).
+        """
+        # Number of genomes we are looking for.
+        N = len(species)
+        logging.info('We are looking for genes common to %s genomes.', N)
+        # Counter of common genes.
+        common = 0
+        for cluster in self.ortho2genes.itervalues():
+            if len(cluster) < N:
+                logging.debug('cluster is too short, only %s genes', len(cluster))
+                continue
+            logging.debug('Cluster is long enough: %s genomes!', len(cluster))
+            found_species = set()
+            for gene in cluster:
+                if self.gene2species(gene) in species:
+                    found_species.add(self.gene2species(gene))
+            if len(found_species) < N:
+                continue
+            else:
+                common += 1
+        return common
+
+
+    def gene2species(self, gene):
+        '''
+        Given a complex gene ID like SAV_1680.BA000030, return the part before the dot
+        (SAV_1680 in this example), which is the LOCUS of the gene's genome.
+        '''
+        return gene[gene.find('.')+1:]
 
     def parse(self):
         logging.info('Reading quick/multi-paranoid gene clusters:')
@@ -47,6 +97,7 @@ class MultiParanoid(object):
                     else:
                         # MultiParanoid
                         idname = 'clusterID'
+                    self.species.add(self.gene2species(row['gene']))
                     # Build gene-to-ortho-cluster-ID(s) dict. row['gene'] is the gene ID.
                     if self.no_tree_conflicts and row['tree_conflict'] != 'No':
                         continue
@@ -62,6 +113,8 @@ class MultiParanoid(object):
             except csv.Error as e:
                 logging.exception('file %s, line %d: %s', self.path, reader.line_num, e)
                 raise
-        print('\ttotal lines read:', reader.line_num)
-        print('\ttotal entries in gene2ortho:', len(self.gene2ortho))
-        print('\ttotal entries in ortho2genes:', len(self.ortho2genes))
+        logging.info('\ttotal lines read: %s', reader.line_num)
+        logging.info('\ttotal species encountered: %s', len(self.species))
+        logging.info('\tlist of these species: %s', self.species)
+        logging.info('\ttotal entries in gene2ortho: %s', len(self.gene2ortho))
+        logging.info('\ttotal entries in ortho2genes: %s', len(self.ortho2genes))
