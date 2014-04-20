4b94fc34a6ecd1a31aa2c1ad60c298b639dc8bc7
Bogdan <to.bogdan@gmail.com>
2014-04-20 18:09:25 +0200

    skip self-self cluster pairs

--- cluster.py
+++ cluster.py
@@ -1111,7 +1111,9 @@ def main():
             for cl1, cl2 in [(cluster(g1, c1), cluster(g2, c2))
                            for c1 in genomes[g1].clusters
                            for c2 in genomes[g2].clusters]:
-                # FIXME: skip self-self clusters.
+                # Skip self-self clusters.
+                if cl1 == cl2:
+                    continue
                 cp = ClusterPair(cl1, cl2)
                 # Calculate the number of orthologous links.
                 cp.assign_orthologous_link(mp, genomes, args)
